17930a3eed3b4fe696aba3c9db2b52fca0e9333d
sirui1 <2420171062@estudiantesunibague.edu.co>
2022-04-26 15:10:52 -0500

    ac

--- src/lowResolutionPcd.py
+++ src/lowResolutionPcd.py
@@ -5,42 +5,38 @@ from params import ParamServer
 
 parSer = ParamServer()
 
-file = ""
-file += parSer.prefix
-file += "pointProyect/data/training/"
-file += parSer.data_file
+pwd_file = ""
+pwd_file += parSer.prefix
+pwd_file += "pointProyect/data/training/"
+file = pwd_file + parSer.data_file
 
 data = pd.read_csv(file, sep=" ", header=0)
-Classification = np.array(data.Classification)
+Classification = np.array(data.Classification)/100
 data = data.drop(['Classification'], axis=1)
 
 pcd_xyz = o3d.geometry.PointCloud()
 pcd_xyz.points = o3d.utility.Vector3dVector(data.to_numpy())
 
-pos_Tree = Classification == 16  # Tree
-pos_ground = Classification == 2  # ground
-pos_Model_keypoints = Classification == 8  # Model_keypoints
-
 colors = np.zeros((data.shape[0], 3))
-colors[pos_Tree, :] = [0.136, 0.556, 0.136]
-colors[pos_ground, :] = [0.512, 0.256, 0]
-colors[pos_Model_keypoints, :] = [0.624, 0.624, 0.624]
+colors[:, 0] = Classification
 
 pcd_xyz.colors = o3d.utility.Vector3dVector(colors)
 print("datos:", len(pcd_xyz.points))
-# o3d.visualization.draw_geometries([pcd_xyz])
 
-# every_k_points = int(input("every_k_points:"))
-# lowPcd_xyz = pcd_xyz.uniform_down_sample(every_k_points)
-# print("low datos:", len(lowPcd_xyz.points))
-# o3d.visualization.draw_geometries([lowPcd_xyz])
-
-every_k_points = float(input("every_k_points:"))
-lowPcd_xyz, idx, lis = pcd_xyz.voxel_down_sample_and_trace(
-    every_k_points, pcd_xyz.get_min_bound(), pcd_xyz.get_max_bound(), False)
+every_k_points = int(input("every_k_points:"))
+lowPcd_xyz = pcd_xyz.uniform_down_sample(every_k_points)
 print("low datos:", len(lowPcd_xyz.points))
-print(idx.shape)
-print(len(lis))
-print(idx[:3])
-print(lis[:3])
-# o3d.visualization.draw_geometries([lowPcd_xyz])
+
+name_x = parSer.data_file[:len(parSer.data_file)-4]+"_low.txt"
+
+file = pwd_file + name_x
+with open(file, 'w') as f:
+    f.write("X Y Z Classification\n")
+    for idx in range(0, len(lowPcd_xyz.points)):
+        x_tmp, y_tmp, z_tmp = lowPcd_xyz.points[idx]
+        Classification_tmp = lowPcd_xyz.colors[idx][0]*100
+
+        f.write(str(x_tmp)+" " +
+                str(y_tmp)+" " +
+                str(z_tmp)+" " +
+                str(Classification_tmp)+"\n")
