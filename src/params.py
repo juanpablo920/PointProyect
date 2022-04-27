0c29170acc2140484af398160ca1aa4c77e07629
juanpablo920 <2420171051@estudiantesunibague.edu.co>
2022-04-27 11:31:48 -0500

    a

--- src/params.py
+++ src/params.py
@@ -3,6 +3,7 @@ class ParamServer:
         # /home/pocampo/
         # /home/sirui/Documents/Johan_Avila/
         # /home/avila/Documentos/
+        # /home/juanocampo/Documentos/Trabajo_de_grado_PointProyect/
         self.prefix = "/home/pocampo/"
 
         # example
