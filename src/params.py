799a99692bbda723ad442ec3055e9332291fc125
juanpablo920 <2420171051@estudiantesunibague.edu.co>
2022-04-27 19:42:04 -0500

    a

--- src/params.py
+++ src/params.py
@@ -8,6 +8,6 @@ class ParamServer:
 
         # example
         # PCD_NIR_training_4M_low10
-        self.data_file = "PCD_NIR_training_4M_low33.txt"
+        self.data_file = "PCD_NIR_training_4M.txt"
 
         self.dsp_types = ["L", "P", "S", "O", "A", "E", "C"]  # Sum
