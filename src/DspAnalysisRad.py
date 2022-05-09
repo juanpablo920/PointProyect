f9ca327d5a0edf4a41c64f90af34d86935880e8e
johan_Avila_U <2420171062@estudiantesunibague.edu.co>
2022-05-09 00:02:11 -0500

    ac

--- src/DspAnalysisRad.py
+++ src/DspAnalysisRad.py
@@ -32,28 +32,35 @@ class dpsAnalysis:
 
         print("datos:", len(self.pcd_xyz.points))
 
-    def init_files_P(self):
-        print("setting_files_P")
+    def setting_files_P123(self):
+        print("setting_files_P123")
         file_base = ""
         file_base += self.parSer.prefix
-        file_base += "pointProyect/dpsAnalysis/radius/data/"
-        file_P123 = file_base+"P123/P_"
-        file_dsp_type = file_base+"dsp_type/"
+        file_base += "pointProyect/dpsAnalysis/radius/data/P123/P_"
 
         for dsp_type in self.parSer.dsp_types:
-
-            file_P123_tmp = file_P123 + dsp_type + ".txt"
-            file_dsp_type_tmp = file_dsp_type + dsp_type + ".txt"
-
-            with open(file_P123_tmp, 'w') as f:
+            file_P123 = file_base + dsp_type + ".txt"
+            with open(file_P123, 'w') as f:
                 f.write("radius P12 P13 P32\n")
 
-            with open(file_dsp_type_tmp, 'w') as f:
-                f.write(dsp_type + "\n")
-
-        file_time = file_P123 + "time.txt"
+        file_time = file_base + "time.txt"
         with open(file_time, 'w') as f:
             f.write("radius time\n")
+
+    def setting_files_dsp(self):
+        print("setting_files_dsp")
+        file_base = ""
+        file_base += self.parSer.prefix
+        file_base += "pointProyect/dpsAnalysis/radius/data/dsp/"
+
+        for dsp_type in self.parSer.dsp_types:
+            file_dsp = file_base + dsp_type + ".txt"
+            with open(file_dsp, 'w') as f:
+                f.write("value\n")
+
+        file_time = file_base + "radius.txt"
+        with open(file_time, 'w') as f:
+            f.write("radius\n")
 
     def calculo_valores_propios(self, matricesCov):
         val_propio_cov = np.linalg.eigvals(matricesCov)
@@ -63,10 +70,10 @@ class dpsAnalysis:
         L3 = val_propio[0]
         if(L3 <= 0):
             L3 = 1e-8
-            if(L2 <= 0):
-                L2 = 1e-8
-                if(L1 <= 0):
-                    L1 = 1e-8
+        if(L2 <= 0):
+            L2 = 1e-8
+        if(L1 <= 0):
+            L1 = 1e-8
         Sum_L123 = (L1+L2+L3)
         e1 = L1/(Sum_L123)
         e2 = L2/(Sum_L123)
@@ -75,33 +82,24 @@ class dpsAnalysis:
 
     def calculo_dsp_type(self, dsp_type, e1, e2, e3):
         dsp_value = 0
-        if(e1 > 0 and e2 > 0 and e3 > 0):
-            if dsp_type == "L":
-                dsp_value = (e1-e2)/(e1)
-            elif dsp_type == "P":
-                dsp_value = (e2-e3)/(e1)
-            elif dsp_type == "S":
-                dsp_value = e3/e1
-            elif dsp_type == "O":
-                dsp_value = np.cbrt(e1*e2*e3)
-            elif dsp_type == "A":
-                dsp_value = (e1-e3)/(e1)
-            elif dsp_type == "E":
-                dsp_value = - (e1*np.log(e1) + e2*np.log(e2) +
-                               e3*np.log(e3))
-            elif dsp_type == "Sum":
-                dsp_value = e1 + e2 + e3
-            elif dsp_type == "C":
-                dsp_value = e3/(e1+e2+e3)
+        if dsp_type == "L":
+            dsp_value = (e1-e2)/(e1)
+        elif dsp_type == "P":
+            dsp_value = (e2-e3)/(e1)
+        elif dsp_type == "S":
+            dsp_value = e3/e1
+        elif dsp_type == "O":
+            dsp_value = np.cbrt(e1*e2*e3)
+        elif dsp_type == "A":
+            dsp_value = (e1-e3)/(e1)
+        elif dsp_type == "E":
+            dsp_value = - (e1*np.log(e1) + e2*np.log(e2) +
+                           e3*np.log(e3))
+        elif dsp_type == "C":
+            dsp_value = e3/(e1+e2+e3)
+        elif dsp_type == "Sum":
+            dsp_value = e1 + e2 + e3
         return dsp_value
-
-    def save_data_dps_type(self, dsp_type, dsp_value):
-        file = ""
-        file += self.parSer.prefix
-        file += "pointProyect/dpsAnalysis/radius/data/dsp_type/"
-        file += dsp_type + ".txt"
-        with open(file, 'a') as f:
-            f.write(str(dsp_value)+"\n")
 
     def save_data_P123_dps_type(self, dsp_type, radius, P12, P13, P32):
         file = ""
@@ -117,10 +115,25 @@ class dpsAnalysis:
         file += self.parSer.prefix
         file += "pointProyect/dpsAnalysis/radius/data/P123/P_time.txt"
         with open(file, 'a') as f:
-            f.write(str(radius)+" " + str(time)+"\n")
+            f.write(str(radius)+" "+str(time)+"\n")
 
-    def radius(self, radius_init, radius_finish, radius_steps):
-        print("radiuss")
+    def save_data_dps_type(self, dsp_type, dsp_value):
+        file = ""
+        file += self.parSer.prefix
+        file += "pointProyect/dpsAnalysis/radius/data/dsp/"
+        file += dsp_type + ".txt"
+        with open(file, 'a') as f:
+            f.write(str(dsp_value)+"\n")
+
+    def save_data_dsp_radius(self, radius):
+        file = ""
+        file += self.parSer.prefix
+        file += "pointProyect/dpsAnalysis/radius/data/dsp/radius.txt"
+        with open(file, 'a') as f:
+            f.write(str(radius)+"\n")
+
+    def generate_files_P123(self, radius_init, radius_finish, radius_steps):
+        print("generate_files_P123")
         for radius in np.arange(radius_init, radius_finish, radius_steps):
             time_inicio = tm.time()
             print(">"*10)
@@ -135,17 +148,13 @@ class dpsAnalysis:
                 e1, e2, e3 = self.calculo_valores_propios(matricesCov_tmp)
                 e.append([e1, e2, e3])
 
-            print("-> dsp_types -> save data P")
+            print("-> save_data_P123_dps_type")
             for dsp_type in self.parSer.dsp_types:
 
                 dsp_value_tmp = [[], [], []]
                 for idx, e_tmp in enumerate(e):
                     e1, e2, e3 = e_tmp
                     dsp_value = self.calculo_dsp_type(dsp_type, e1, e2, e3)
-                    self.save_data_dps_type(dsp_type, dsp_value)
-
-                    if(e1 < 0 or e2 < 0 or e3 < 0):
-                        continue
 
                     if self.Classification[idx] == 16:  # Tree
                         dsp_value_tmp[0].append(dsp_value)
@@ -172,35 +181,38 @@ class dpsAnalysis:
                     np.abs(marcador_mean - ground_mean)) / (3*(marcador_std + ground_std))
                 self.save_data_P123_dps_type(dsp_type, radius, P12, P13, P32)
 
-                # b1 = dsp_value_tmp[0]  # arbol
-                # b2 = dsp_value_tmp[1]
-                # b3 = dsp_value_tmp[2]
-                # b1.sort()
-                # b2.sort()
-                # b3.sort()
-
-                # Clase1dls = norm.pdf(b1, tree_mean, tree_std)
-                # Clase2dls = norm.pdf(b2, ground_mean, ground_std)
-                # Clase3dls = norm.pdf(b3, marcador_mean, marcador_std)
-                # plt.plot(b1, Clase1dls, 'b', label="Arbol")
-                # plt.plot(b2, Clase2dls, 'r', label="Tierra")
-                # plt.plot(b3, Clase3dls, 'g', label="Marcador")
-                # plt.title('Distribucion normal Planaridad')
-                # plt.legend()
-                # plt.show()
-
             e = None
             dsp_value_tmp = None
             time = tm.time() - time_inicio
             self.save_data_P123_time(radius, time)
 
-        # plt.hist(self.parSer.a, bins=255)
-        # plt.show()
+    def generate_files_dsp(self, radius):
+        print("generate_files_dsp")
+        print(">"*10)
+        print("-> radius: ", radius)
+        print("-> calculo de matrices de covarianza")
+        self.pcd_xyz.estimate_covariances(
+            search_param=o3d.geometry.KDTreeSearchParamRadius(radius=radius))
 
-    def graph_P_dps_type(self, dps_type, P12, P13, P32, radius):
+        print("-> calculo valores propios e")
+        e = []
+        for matricesCov_tmp in self.pcd_xyz.covariances:
+            e1, e2, e3 = self.calculo_valores_propios(matricesCov_tmp)
+            e.append([e1, e2, e3])
+
+        print("-> save_data_dps_type")
+        for dsp_type in self.parSer.dsp_types:
+            for e_tmp in e:
+                e1, e2, e3 = e_tmp
+                dsp_value = self.calculo_dsp_type(dsp_type, e1, e2, e3)
+                self.save_data_dps_type(dsp_type, dsp_value)
+
+        self.save_data_dsp_radius(radius)
+
+    def graph_P123_dps_type(self, dps_type, P12, P13, P32, radius):
         pwd_imagen = ""
         pwd_imagen += self.parSer.prefix
-        pwd_imagen += "pointProyect/dpsAnalysis/radius/images/graphics_P/"
+        pwd_imagen += "pointProyect/dpsAnalysis/radius/images/graphics_P123/"
 
         max_P12 = np.amax(P12)
         max_P13 = np.amax(P13)
@@ -234,10 +246,25 @@ class dpsAnalysis:
         plt.savefig(pwd_imagen + dps_type + "_vs_radius.png")
         plt.clf()
 
-    def graph_average_P_dps_type(self, dps_type, P12, P13, P32, radius):
+    def graph_time_P(self, time, radius):
         pwd_imagen = ""
         pwd_imagen += self.parSer.prefix
-        pwd_imagen += "pointProyect/dpsAnalysis/radius/images/average_P/"
+        pwd_imagen += "pointProyect/dpsAnalysis/radius/images/graphics_P123/"
+
+        plt.figure()
+        plt.plot(radius, time, "C0", label="time")
+        plt.title("time_vs_radius")
+        plt.xlabel('radius')
+        plt.ylabel('time')
+        plt.grid(True)
+        plt.legend()
+        plt.savefig(pwd_imagen + "time_vs_radius.png")
+        plt.clf()
+
+    def graph_average_P123_dps_type(self, dps_type, P12, P13, P32, radius):
+        pwd_imagen = ""
+        pwd_imagen += self.parSer.prefix
+        pwd_imagen += "pointProyect/dpsAnalysis/radius/images/average_P123/"
 
         average_P = (P12 + P13 + P32)/3
         max_average = np.amax(average_P)
@@ -279,44 +306,20 @@ class dpsAnalysis:
         plt.savefig(pwd_imagen + "averages_P_vs_radius.png")
         plt.clf()
 
-    def graph_time_P(self, time, radius):
-        pwd_imagen = ""
-        pwd_imagen += self.parSer.prefix
-        pwd_imagen += "pointProyect/dpsAnalysis/radius/images/graphics_P/"
+    def generate_graphics_P123(self):
+        pwd_files = ""
+        pwd_files += self.parSer.prefix
+        pwd_files += "pointProyect/dpsAnalysis/radius/data/P123/"
 
-        plt.figure()
-        plt.plot(radius, time, "C0", label="time")
-        plt.title("time_vs_radius")
-        plt.xlabel('radius')
-        plt.ylabel('time')
-        plt.grid(True)
-        plt.legend()
-        plt.savefig(pwd_imagen + "time_vs_radius.png")
-        plt.clf()
-
-    def graphics(self):
-        print("graphics")
-
-        pwd_files_base = ""
-        pwd_files_base += self.parSer.prefix
-        pwd_files_base += "pointProyect/dpsAnalysis/radius/data/"
-        
-        pwd_files_P123 = pwd_files_base+"P123/P_"
-        pwd_files_dsp_type = pwd_files_base+"dsp_type/"
-
-        name_files_P123 = os.listdir(pwd_files_P123)
-        name_files_P123.remove("data.txt")
-        name_files_P123.remove("P_time.txt")
-
-        name_files_dsp_type = os.listdir(pwd_files_dsp_type)
-        name_files_dsp_type.remove("data.txt")
+        name_files = os.listdir(pwd_files)
+        name_files.remove("data.txt")
+        name_files.remove("P_time.txt")
 
         list_averages_P = []
-        for name_file_P123 in name_files_P123:
+        for name_file in name_files:
             data = pd.read_csv(pwd_files+name_file, sep=" ", header=0)
             print("-"*10)
             print("name_file: ", name_file)
-            print(data)
 
             radius = np.array(data.radius)
 
@@ -328,8 +331,8 @@ class dpsAnalysis:
             P32 = np.array(data.P32)
 
             dps_type = name_file[:len(name_file)-4]
-            self.graph_P_dps_type(dps_type, P12, P13, P32, radius)
-            average_P_tmp = self.graph_average_P_dps_type(
+            self.graph_P123_dps_type(dps_type, P12, P13, P32, radius)
+            average_P_tmp = self.graph_average_P123_dps_type(
                 dps_type, P12, P13, P32, radius)
 
             list_averages_P.append(average_P_tmp)
@@ -343,43 +346,119 @@ class dpsAnalysis:
         time = np.array(times.time)
         self.graph_time_P(time, radius)
 
-        # averages_P = list_averages_P[0]
-        # for i in range(1, len(list_averages_P)):
-        #     averages_P = averages_P + list_averages_P[i]
-
         averages_P = np.sum(list_averages_P, axis=0)/len(list_averages_P)
         self.graph_selection_Radius(averages_P, radius)
+
+    def graph_gaussiana_dps_type(self, dps_type, dsp_tree, dsp_ground, dsp_Model_keypoints):
+        pwd_imagen = ""
+        pwd_imagen += self.parSer.prefix
+        pwd_imagen += "pointProyect/dpsAnalysis/radius/images/gaussiana_dps/"
+
+        # b1 = dsp_value_tmp[0]
+        # b2 = dsp_value_tmp[1]
+        # b3 = dsp_value_tmp[2]
+        # b1.sort()
+        # b2.sort()
+        # b3.sort()
+
+        # Clase1dls = norm.pdf(b1, tree_mean, tree_std)
+        # Clase2dls = norm.pdf(b2, ground_mean, ground_std)
+        # Clase3dls = norm.pdf(b3, marcador_mean, marcador_std)
+        # plt.plot(b1, Clase1dls, 'b', label="Arbol")
+        # plt.plot(b2, Clase2dls, 'r', label="Tierra")
+        # plt.plot(b3, Clase3dls, 'g', label="Marcador")
+        # plt.title('Distribucion normal Planaridad')
+        # plt.legend()
+        # plt.show()
+
+        # plt.hist(self.parSer.a, bins=255)
+        # plt.show()
+
+    def generate_graphics_dsp(self):
+        pwd_files = ""
+        pwd_files += self.parSer.prefix
+        pwd_files += "pointProyect/dpsAnalysis/radius/data/dsp"
+
+        name_files = os.listdir(pwd_files)
+        name_files.remove("data.txt")
+        name_files.remove("radius.txt")
+
+        for name_file in name_files:
+            data = pd.read_csv(pwd_files+name_file, sep=" ", header=0)
+            print("-"*10)
+            print("name_file: ", name_file)
+
+            dsp_values = np.array(data.value)
+
+            pos_Tree = self.Classification == 16  # Tree
+            pos_ground = self.Classification == 2  # ground
+            pos_Model_keypoints = self.Classification == 8  # Model_keypoints
+
+            dsp_tree = dsp_values[pos_Tree]
+            dsp_ground = dsp_values[pos_ground]
+            dsp_Model_keypoints = dsp_values[pos_Model_keypoints]
+
+            dps_type = name_file[:len(name_file)-4]
+            self.graph_gaussiana_dps_type(dps_type,dsp_tree,dsp_ground,dsp_Model_keypoints)
 
 
 if __name__ == '__main__':
     dps_analysis = dpsAnalysis()
 
-    print("Opcion_1: generar archivos P")
-    print("Opcion_2: graficas")
+    print("Opcion_1: generar archivos P123")
+    print("Opcion_2: generar graficas P123")
+    print("")
+    print("Opcion_3: generar archivos dps")
+    print("Opcion_4: generar graficas dps")
+    print("")
 
     opcion = input("opcion: ")
 
     if opcion == "1":
         print("="*10)
-        print("generar archivos P")
-        print("-"*10)
-        dps_analysis.read_data()
-        print("-"*10)
-        print("Opcion_1: init_files_P")
+        print("generar archivos P123")
+        print("")
+        print("Opcion_1: init_files_P123")
         print("Opcion_x: continue")
         opcion = input("opcion: ")
         if opcion == "1":
-            dps_analysis.init_files_P()
+            dps_analysis.setting_files_P123()
+        print("")
+        dps_analysis.read_data()
         print("-"*10)
         radius_init = float(input("radius_init: "))
         radius_finish = float(input("radius_finish: "))
         radius_steps = float(input("radius_steps: "))
         print("-"*10)
-        dps_analysis.radius(radius_init, radius_finish, radius_steps)
+        dps_analysis.generate_files_P123(
+            radius_init, radius_finish, radius_steps)
     elif opcion == "2":
         print("="*10)
-        print(" graficas P")
-        dps_analysis.graphics()
+        print("generar graficas P123")
+        dps_analysis.generate_graphics_P123()
+        print("")
+    elif opcion == "3":
+        print("="*10)
+        print("generar archivos dsp")
+        print("")
+        print("Opcion_1: continue")
+        print("Opcion_x: salir")
+        opcion = input("opcion: ")
+        if opcion != "1":
+            exit()
+        print("")
+        dps_analysis.setting_files_dsp()
+        dps_analysis.read_data()
+        print("")
+        radius_dsp = float(input("radius: "))
+        print("")
+        dps_analysis.generate_files_dsp(radius_dsp)
+    elif opcion == "4":
+        print("="*10)
+        print("generar graficas dsp")
+        dps_analysis.read_data()
+        dps_analysis.generate_graphics_dsp()
+        print("")
     else:
         print("="*10)
         print("no es una opcion '{opcion}'")
