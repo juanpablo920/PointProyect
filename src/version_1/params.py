15e9a8b36a23ee41e96e09d7b4ac0d6ec2b1c269
sirui1 <2420171062@estudiantesunibague.edu.co>
2022-07-06 12:40:49 -0500

    ac

---
+++ src/version_1/params.py
@@ -0,0 +1,19 @@
+class ParamServer:
+    def __init__(self):
+        # /home/pocampo/
+        # /home/sirui/pi_ws/src/
+        # /home/avila/Documentos/
+        # /home/juanpablo
+        self.prefix = "/home/pocampo/"
+
+        # example_train
+        # PCD_NIR_training_4M_low50
+
+        self.dsp_types = ["L", "P", "S", "O", "A", "E", "C"]  # Sum
+
+        self.data_file_train = "Libano_coffee_etiquetado_training.txt"
+        self.data_file_valid = "Libano_coffee_etiquetado_validation.txt"
+
+        self.clf_P_train = 0.7  # Porcentaje de particion
+        self.clf_P_adjust = (1-self.clf_P_train)/(2)
+        self.clf_P_validation = self.clf_P_adjust
