8123bfc134c71f82622b568f1b961dbc60acd8bb
juanpablo920 <2420171051@estudiantesunibague.edu.co>
2022-07-11 16:23:40 -0500

    ac

--- src/version_2/Clustering.py
+++ src/version_2/Clustering.py
@@ -11,6 +11,7 @@ from scipy.stats import norm
 from sklearn.metrics import silhouette_score
 from sklearn.cluster import KMeans
 
+
 class clustering:
 
     def __init__(self):
@@ -43,22 +44,27 @@ class clustering:
         # plt.show()
 
         #X = np.array(list(zip(x1, x2))).reshape(len(x1),2)
-        
+
         print("cluster")
-        silueta = []
-        for k in range(2, 50):
-            print("K: ",k)
-            kmeans = KMeans(n_clusters = k).fit(self.pcd)
-            labels = kmeans.labels_
-            print(labels)
-            silueta.append(silhouette_score(self.pcd, labels, metric = 'euclidean'))
-            print(silueta)
-           
-        plt.plot(range(2, 11), silueta, 'bx-')
+        # silueta = []
+        # for k in range(2, 50):
+        #     print("K: ", k)
+        #     kmeans = KMeans(n_clusters=k).fit(self.pcd)
+        #     labels = kmeans.labels_
+        #     print(labels)
+        #     silueta.append(silhouette_score(
+        #         self.pcd, labels, metric='euclidean'))
+        #     print(silueta)
+
+        silueta = [0.4600763203672339, 0.4791886811848334, 0.45641667422853693, 0.43899880968732624, 0.472847239005536, 0.4708187515426087, 0.46530791591702203, 0.4930105134753089, 0.5063025823537268, 0.5166899805704104, 0.5181851286456105, 0.5279329279863526, 0.5342069772920724, 0.5382661258559718, 0.5414265414179411, 0.5355857779002761, 0.5129431245523784, 0.5197695411559515, 0.5091491017005373, 0.5080775124640468, 0.5061471482411569, 0.4894572036084337, 0.473635111504707, 0.4767245048442456,
+                   0.47744463833828826, 0.46770420517806266, 0.48594549618107924, 0.46000095667299895, 0.466268358464164, 0.4459063003377373, 0.4616430328141441, 0.45718445548128905, 0.4524792282339751, 0.4496814515545284, 0.44326407298234644, 0.44824244257494533, 0.43535236913172565, 0.43220757035473345, 0.42938324929099086, 0.42652773593274457, 0.42282234017733933, 0.427682207301929, 0.42007629833667887, 0.42402261607728986, 0.4139103795358925, 0.3738628578112873, 0.40080944896084975, 0.3883825791500647]
+
+        plt.plot(range(2, 50), silueta, 'bx-')
         plt.xlabel('Clústeres')
         plt.ylabel('Puntaje de la silueta')
         plt.title('Metodo de la Silueta')
         plt.savefig("Silueta_vs_Clases.png")
+
 
 if __name__ == '__main__':
     Cluster = clustering()
