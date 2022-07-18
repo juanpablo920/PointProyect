984c50fb21e23ab8073b2b88df98456c25621d9d
juanpablo920 <2420171051@estudiantesunibague.edu.co>
2022-07-18 17:33:55 -0500

    a

--- src/version_2/params.py
+++ src/version_2/params.py
@@ -12,7 +12,7 @@ class ParamServer:
         self.dsp_types = ["L", "P", "S", "O", "A", "E", "C"]  # Sum
 
         self.data_file_train = "coffee_citricos_training_34arboles.txt"
-        self.data_file_valid = "Libano_coffee_etiquetado_validation_15arboles.txt"
+        self.data_file_valid = "Libano_coffee_etiquetado_validation_15arboles_low_10.txt"
 
         self.clf_P_train = 0.7  # Porcentaje de particion
         self.clf_P_adjust = (1-self.clf_P_train)/(2)
