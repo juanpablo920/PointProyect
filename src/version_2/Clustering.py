38c1f895a5ce4d31641bef1c02bd49a82c467ced
juanpablo920 <2420171051@estudiantesunibague.edu.co>
2022-07-19 00:00:29 -0500

    a

--- src/version_2/Clustering.py
+++ src/version_2/Clustering.py
@@ -1,16 +1,12 @@
-import os
 import numpy as np
-import open3d as o3d
 import pandas as pd
 import matplotlib.pyplot as plt
-import time as tm
 
 from sklearn import cluster
 from params import ParamServer
 from scipy.stats import norm
-from sklearn.metrics import silhouette_score
+from sklearn.metrics import silhouette_samples, silhouette_score
 from sklearn.cluster import KMeans
-
 
 class clustering:
 
@@ -38,17 +34,6 @@ class clustering:
 
     def cluster(self):
 
-        # x1 =np.array([3, 1, 1, 2, 1, 6, 6, 6, 5, 6, 7, 8, 9, 8, 9, 9, 8])
-        # x2 =np.array([5, 4, 5, 6, 5, 8, 6, 7, 6, 7, 1, 2, 1, 2, 3, 2, 3])
-        # plt.plot()
-        # plt.xlim([0,10])
-        # plt.ylim([0,10])
-        # plt.title( 'Conjunto de Datos')
-        # plt.scatter(x1, x2)
-        # plt.show()
-
-        #X = np.array(list(zip(x1, x2))).reshape(len(x1),2)
-
         print("cluster")
         silueta = []
         for k in range(2, 50):
@@ -59,9 +44,11 @@ class clustering:
             silueta.append(silhouette_score(
                 self.pcd, labels, metric='euclidean'))
             print(silueta)
-
-        #silueta = [0.4600763203672339, 0.4791886811848334, 0.45641667422853693, 0.43899880968732624, 0.472847239005536, 0.4708187515426087, 0.46530791591702203, 0.4930105134753089, 0.5063025823537268, 0.5166899805704104, 0.5181851286456105, 0.5279329279863526, 0.5342069772920724, 0.5382661258559718, 0.5414265414179411, 0.5355857779002761, 0.5129431245523784, 0.5197695411559515, 0.5091491017005373, 0.5080775124640468, 0.5061471482411569, 0.4894572036084337, 0.473635111504707, 0.4767245048442456,
-                   #0.47744463833828826, 0.46770420517806266, 0.48594549618107924, 0.46000095667299895, 0.466268358464164, 0.4459063003377373, 0.4616430328141441, 0.45718445548128905, 0.4524792282339751, 0.4496814515545284, 0.44326407298234644, 0.44824244257494533, 0.43535236913172565, 0.43220757035473345, 0.42938324929099086, 0.42652773593274457, 0.42282234017733933, 0.427682207301929, 0.42007629833667887, 0.42402261607728986, 0.4139103795358925, 0.3738628578112873, 0.40080944896084975, 0.3883825791500647]
+        
+        silueta = np.max(silueta)
+        print(silueta)
+        self.N_arbol=labels[silueta]
+        print(self.N_arbol)
 
         aa = list(range(2, 50))
 
@@ -74,6 +61,28 @@ class clustering:
         plt.title('Metodo de la Silueta')
         #plt.show()
         plt.savefig("Silueta_vs_Clases.png")
+
+    def Classification(self):
+        
+        df=pd.read_csv("/home/hmurcia/Downloads/libano.txt",sep=" ")
+        X=np.zeros([len(df),2])
+        X[:,0]=df.X.values
+        X[:,1]=df.Y.values
+        kmeans = KMeans(n_clusters=self.N_arbol, random_state=0)
+        kmeans = kmeans.fit(X)
+        centers=kmeans.cluster_centers_
+        plt.figure()
+        plt.plot(X[:,0],X[:,1],'.',label='data')
+        plt.plot(centers[:,0],centers[:,1],'*',label="Kmenas centers")
+        plt.legend()
+        plt.show()
+
+        pred = kmeans.predict(X)
+        plt.figure()
+        plt.scatter(X[:,0],X[:,1], c = pred)
+        plt.plot(centers[:,0],centers[:,1],'*',label="Kmenas centers")
+        plt.legend()
+        plt.show()
 
 
 if __name__ == '__main__':
--- src/version_2/params.py
+++ src/version_2/params.py
@@ -4,15 +4,15 @@ class ParamServer:
         # /home/sirui/pi_ws/src/
         # /home/avila/Documentos/
         # /home/juanpablo/
-        self.prefix = "/home/pocampo/"
+        self.prefix = "/home/juanpablo/"
 
         # example_train
         # PCD_NIR_training_4M_low50
 
         self.dsp_types = ["L", "P", "S", "O", "A", "E", "C"]  # Sum
 
-        self.data_file_train = "PCD_NIR_training_4M_arbol_suelo.txt"
-        self.data_file_valid = "PCD_NIR_validation_4M_arbol_suelo.txt"
+        self.data_file_train = "Libano_coffee_etiquetado_training_34arboles.txt"
+        self.data_file_valid = "Libano_coffee_etiquetado_validation_15arboles.txt"
 
         self.clf_P_train = 0.7  # Porcentaje de particion
         self.clf_P_adjust = (1-self.clf_P_train)/(2)
