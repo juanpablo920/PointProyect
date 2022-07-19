85626ef3789a4771e926cac7a10ee6b3b1b1bc25
juanpablo920 <2420171051@estudiantesunibague.edu.co>
2022-07-19 13:39:53 -0500

    a

--- src/version_2/params.py
+++ src/version_2/params.py
@@ -4,7 +4,7 @@ class ParamServer:
         # /home/sirui/pi_ws/src/
         # /home/avila/Documentos/
         # /home/juanpablo/
-        self.prefix = "/home/juanpablo/"
+        self.prefix = "/home/pocampo/"
 
         # example_train
         # PCD_NIR_training_4M_low50
@@ -13,6 +13,9 @@ class ParamServer:
 
         self.data_file_train = "Libano_coffee_etiquetado_training_34arboles.txt"
         self.data_file_valid = "Libano_coffee_etiquetado_validation_15arboles.txt"
+
+        self.data_file_train_clustering = "Libano_coffee_etiquetado_validation_15arboles_low_10.txt"
+        self.data_file_valid_clustering = "Libano_coffee_etiquetado_validation_15arboles_low_10.txt"
 
         self.clf_P_train = 0.7  # Porcentaje de particion
         self.clf_P_adjust = (1-self.clf_P_train)/(2)
