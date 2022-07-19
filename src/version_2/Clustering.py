d9e401dfe2c55678e85930d0e43edf4c6187ab8b
juanpablo920 <2420171051@estudiantesunibague.edu.co>
2022-07-19 13:08:05 -0500

    a

--- src/version_2/Clustering.py
+++ src/version_2/Clustering.py
@@ -13,6 +13,25 @@ class clustering:
     def __init__(self):
         self.parSer = ParamServer()
 
+    def read_data_clustering(self):
+        print("read_data_clustering")
+        file_clustering = ""
+        file_clustering += self.parSer.prefix
+        file_clustering += "pointProyect/data/validation/"
+        file_clustering += self.parSer.data_file_valid_clustering
+
+        data_clustering = pd.read_csv(file_clustering, sep=" ", header=0)
+        self.Classification = np.array(data_clustering.Classification)
+        
+        indices = data_clustering[data_clustering['Classification'] == 2].index
+        data_clustering = data_clustering.drop(indices)
+
+        self.pcd_clustering = data_clustering.to_numpy()
+        print(self.pcd_clustering)
+
+        print("datos:", self.pcd_clustering.shape)
+        print("File: ", file_clustering)
+
     def read_data(self):
         print("read_data")
         file = ""
@@ -38,15 +57,15 @@ class clustering:
         silueta = []
         for k in range(2, 50):
             print("K: ", k)
-            kmeans = KMeans(n_clusters=k).fit(self.pcd)
+            kmeans = KMeans(n_clusters=k).fit(self.pcd_clustering)
             labels = kmeans.labels_
             print(labels)
             silueta.append(silhouette_score(
-                self.pcd, labels, metric='euclidean'))
+                self.pcd_clustering, labels, metric='euclidean'))
             print(silueta)
         
-        silueta = np.max(silueta)
-        print(silueta)
+        max_silueta = np.max(silueta)
+        print(max_silueta)
         self.N_arbol=labels[silueta]
         print(self.N_arbol)
 
@@ -62,27 +81,27 @@ class clustering:
         #plt.show()
         plt.savefig("Silueta_vs_Clases.png")
 
-    def Classification(self):
+    # def Classification(self):
         
-        df=pd.read_csv("/home/hmurcia/Downloads/libano.txt",sep=" ")
-        X=np.zeros([len(df),2])
-        X[:,0]=df.X.values
-        X[:,1]=df.Y.values
-        kmeans = KMeans(n_clusters=self.N_arbol, random_state=0)
-        kmeans = kmeans.fit(X)
-        centers=kmeans.cluster_centers_
-        plt.figure()
-        plt.plot(X[:,0],X[:,1],'.',label='data')
-        plt.plot(centers[:,0],centers[:,1],'*',label="Kmenas centers")
-        plt.legend()
-        plt.show()
+    #     df=pd.read_csv("/home/hmurcia/Downloads/libano.txt",sep=" ")
+    #     X=np.zeros([len(df),2])
+    #     X[:,0]=df.X.values
+    #     X[:,1]=df.Y.values
+    #     kmeans = KMeans(n_clusters=self.N_arbol, random_state=0)
+    #     kmeans = kmeans.fit(X)
+    #     centers=kmeans.cluster_centers_
+    #     plt.figure()
+    #     plt.plot(X[:,0],X[:,1],'.',label='data')
+    #     plt.plot(centers[:,0],centers[:,1],'*',label="Kmenas centers")
+    #     plt.legend()
+    #     plt.show()
 
-        pred = kmeans.predict(X)
-        plt.figure()
-        plt.scatter(X[:,0],X[:,1], c = pred)
-        plt.plot(centers[:,0],centers[:,1],'*',label="Kmenas centers")
-        plt.legend()
-        plt.show()
+    #     pred = kmeans.predict(X)
+    #     plt.figure()
+    #     plt.scatter(X[:,0],X[:,1], c = pred)
+    #     plt.plot(centers[:,0],centers[:,1],'*',label="Kmenas centers")
+    #     plt.legend()
+    #     plt.show()
 
 
 if __name__ == '__main__':
